8e84cd7a6f46c6e6752b2bffacf0a9f7380a5342
yusx-swapp <yusx@iastate.edu>
2024-01-19 00:09:28 -0600

    remove load_best_model_at_end

--- scripts/eval_vit.py
+++ scripts/eval_vit.py
@@ -60,18 +60,18 @@ model = ViTForImageClassification.from_pretrained(
 training_args = TrainingArguments(
     output_dir="./eval/vit-cifar100",
     per_device_train_batch_size=16,
-    per_device_eval_batch_size=512,
+    per_device_eval_batch_size=2048,
     evaluation_strategy="no",
     num_train_epochs=4,
     # save_steps=100,
     # eval_steps=100,
     # logging_steps=10,
     learning_rate=2e-4,
-    save_total_limit=2,
+    # save_total_limit=2,
     remove_unused_columns=False,
     push_to_hub=False,
     # report_to="tensorboard",
-    load_best_model_at_end=True,
+    # load_best_model_at_end=True,
     dataloader_num_workers=8,
 )
 
