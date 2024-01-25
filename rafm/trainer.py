63898b1b8543a72e75b0dd26069aa517b3232673
yusx-swapp <yusx@iastate.edu>
2024-01-24 19:51:51 -0600

    kd trainer

--- rafm/trainer.py
+++ rafm/trainer.py
@@ -4,7 +4,6 @@ import time
 import numpy as np
 from .utils import EarlyStopping, step_lr
 from .modeling_rafm import RAFM
-from torch.utils.tensorboard import SummaryWriter
 from transformers import (
     TrainingArguments,
     Trainer,
@@ -13,11 +12,59 @@ from transformers import (
     DataCollatorForSeq2Seq,
     EvalPrediction,
 )
-
-from tqdm import tqdm
-
+from transformers import TrainingArguments, Trainer
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+
+class ImageDistilTrainer(Trainer):
+    def __init__(
+        self,
+        teacher_model=None,
+        student_model=None,
+        temperature=None,
+        lambda_param=None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(model=student_model, *args, **kwargs)
+        self.teacher = teacher_model
+        self.student = student_model
+        self.loss_function = nn.KLDivLoss(reduction="batchmean")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.teacher.to(device)
+        self.teacher.eval()
+        self.temperature = temperature
+        self.lambda_param = lambda_param
+
+    def compute_loss(self, student, inputs, return_outputs=False):
+        student_output = self.student(**inputs)
+
+        with torch.no_grad():
+            teacher_output = self.teacher(**inputs)
+
+        # Compute soft targets for teacher and student
+        soft_teacher = F.softmax(teacher_output.logits / self.temperature, dim=-1)
+        soft_student = F.log_softmax(student_output.logits / self.temperature, dim=-1)
+
+        # Compute the loss
+        distillation_loss = self.loss_function(soft_student, soft_teacher) * (
+            self.temperature**2
+        )
+
+        # Compute the true label loss
+        student_target_loss = student_output.loss
+
+        # Calculate final loss
+        loss = (
+            1.0 - self.lambda_param
+        ) * student_target_loss + self.lambda_param * distillation_loss
+        return (loss, student_output) if return_outputs else loss
 
 
 def get_optimizer_and_scheduler(model, lr):
